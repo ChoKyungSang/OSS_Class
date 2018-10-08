3b18efc910d314bbaebabd25647a76fbfc9eaaf5
김기범 <jimi567@kookmin.ac.kr>
2018-10-08 07:07:34 +0000

    schedule model  choice set make

--- Birds_Eye/Birds_Eye/scheduleTable/views.py
+++ Birds_Eye/Birds_Eye/scheduleTable/views.py
@@ -9,4 +9,4 @@ from django.utils import timezone
 
 def post_list(request):
     schedules = Schedule.objects.all()
-    return render(request, 'blog/post_list.html',  {'schedules': schedules})
+    return render(request, 'blog/post_list.html',   {'schedules': schedules})
